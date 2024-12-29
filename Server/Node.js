acd2c73a47b9d66942322a90846bc719c534e7ab
pavan <pashampavan02@gmail.com>
2024-12-29 14:40:52 +0530

    Global Hooks Conext are added

--- Server/Node.js
+++ Server/Node.js
@@ -48,8 +48,9 @@ if (!isPasswordValid) {
 
   // Generate JWT token
   const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, {
-    expiresIn: "1h",
+    expiresIn: "15m",
   });
+  
 
   res.json({ token });
 });
